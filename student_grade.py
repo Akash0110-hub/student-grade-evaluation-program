2ce194adbf131c955a5c804e1070fb9f4e243e92
akashshettar <akashgshettar10@gmail.com>
2025-10-04 09:07:31 +0530

    save

--- student_grade.py
+++ student_grade.py
@@ -15,4 +15,29 @@ else:
 
 print("Grade:", grade)
 
+# student_grade.py
 
+def calculate_grade(avg):
+    if avg >= 90:
+        return "A"
+    elif avg >= 75:
+        return "B"
+    elif avg >= 60:
+        return "C"
+    elif avg >= 40:
+        return "D"
+    else:
+        return "Fail"
+
+# Take input for 5 subjects
+marks = []
+for i in range(1, 6):
+    m = int(input(f"Enter marks for subject {i}: "))
+    marks.append(m)
+
+average = sum(marks) / 5
+grade = calculate_grade(average)
+
+print("Marks:", marks)
+print(f"Average: {average:.2f}")
+print(f"Grade: {grade}")
